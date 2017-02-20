735a753212068a96968900ec4b0fff5961011d8a
Leloz Suliman <leloz997@gmail.com>
2017-02-20 23:22:15 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+	return true;
+}
+
+function sayHey(){
+	return "Hey!";
+}
+
+function sayHeyFriend(name){
+	return `Hey, ${name}!`; 
+}
+
+function favoriteIceCream(namefav){
+	return `I love ${namefav}`;
+}
+
+function shouting(s){
+	return s.toUpperCase();
+}
+
+function roundDown(num){
+return Math.floor(num);
+}
